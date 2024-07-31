521b24e9a5087f4321d8794c7b9047d1caad2155
Josh Watts <josh@sroz.net>
2024-07-31 11:13:28 -0400

    Initial release

---
+++ backend.js
@@ -0,0 +1,112 @@
+const http = require('node:http');
+const fs = require('node:fs');
+const Ajv = require('ajv');
+const ajv = new Ajv();
+const board_file_name = "/var/lib/race/board.json";
+const validate_board = ajv.compile({
+	type: "object",
+	properties: {
+		ver: {type: "integer", const: 1},
+		drivers: {
+			type: "array",
+			items: {
+				type: "object",
+				properties: {
+					name: {
+						type: "string",
+						maxLength: 128
+					},
+					time: {type: "number"}
+				},
+				required: [
+					"name",
+					"time"
+				],
+				additionalProperties: false
+			},
+			maxItems: 128
+		}
+	},
+	required: [
+		"ver",
+		"drivers"
+	],
+	additionalProperties: false
+});
+
+function serve_file(res, fn, type) {
+	let st;
+	try {
+		st = fs.statSync(fn);
+	} catch (err) {
+		let empty = "{}";
+		res.writeHead(200, {
+			'Content-Type': type,
+			'Content-Length': empty.length,
+		});
+		res.write(empty);
+		res.end();
+		return;
+	}
+	res.writeHead(200, {
+		'Content-Type': type,
+		'Content-Length': st.size
+	});
+	var readStream = fs.createReadStream(fn);
+	readStream.pipe(res);
+}
+
+function write_file(req, res, fn) {
+	let body = null;
+	req.on('data', (chunk) => {
+		if (body == null) {
+			body = chunk.toString();
+			if (chunk.length > 2*1024) {
+				res.statusCode = 400;
+				res.end();
+			}
+		}
+	}).on('end', () => {
+		let board = JSON.parse(body);
+		if (!validate_board(board)) {
+			res.statusCode = 400;
+			res.end();
+			return;
+		}
+		board.drivers = board.drivers.map((d) => {
+			d.name = d.name.replace(/[^A-Za-z\s]/g, '');
+			return d;
+		});
+		console.log(Date.now(), 'WRITE', board);
+		fs.writeFileSync(fn, JSON.stringify(board));
+	});
+}
+
+const s = http.createServer();
+s.on('request', (req, res) => {
+	try {
+		console.log(Date.now(), req.method, req.url);
+		if (req.url == "/board") {
+			if (req.method == "GET") {
+				serve_file(res, board_file_name, "application/json");
+			} else if (req.method == "POST") {
+				write_file(req, res, board_file_name);
+			} else {
+				res.statusCode = 400;
+				res.end();
+			}
+		} else if (req.url == "/" || req.url == "/index.html") {
+			serve_file(res, "./index.html", "text/html");
+		} else if (req.url == "/" || req.url == "/index.js") {
+			serve_file(res, "./index.js", "application/javascript");
+		} else {
+			res.statusCode = 404;
+			res.end();
+		}
+	} catch(err) {
+		console.log(500, err);
+		res.statusCode = 500;
+		res.end();
+	}
+});
+s.listen(8000);
---
+++ index.js
@@ -0,0 +1,165 @@
+window.addEventListener('load', () => {
+    class Driver {
+        constructor(params) {
+            if (!params) {
+                params = {};
+            }
+            const driver_template = document.querySelector("#driver_template");
+            let e = driver_template.content.cloneNode(true);
+            this.e_li = e.querySelector("li.driver");
+            this.e_name = e.querySelector("#name");
+            this.e_time = e.querySelector("#time");
+            this.e_delete = e.querySelector("#del_driver");
+            if ("name" in params) {
+                this.name = params.name;
+            } else {
+                this.name = "";
+            }
+            if ("time" in params) {
+                this.time = params.time;
+            } else {
+                this.time = 0.0;
+            }
+            e.querySelectorAll("input").forEach((v, i, l) => {
+                v.addEventListener('change', (e) => { this.change(e); });
+            });
+            this.e_delete.addEventListener('click', (e) => { window.lb.del_driver(this); });
+        }
+
+        static timeToString(t) {
+            if (!t) {
+                return "0.000";
+            }
+            let v;
+            let m = Math.floor(t / 60.0);
+            t -= m * 60.0;
+            let s = Math.floor(t);
+            t -= s;
+            let ms = Math.floor(t * 1000);
+            if (m > 0) {
+                v = m.toString() + ":" + s.toString().padStart(2, '0') + ".";
+            } else {
+                v = s.toString() + ".";
+            }
+            v += ms.toString().padStart(3, '0');
+            return v;
+        }
+
+        static stringToTime(v) {
+            if (!v) {
+                return 0.0;
+            }
+            let t = 0.0;
+            let p;
+            p = v.split(':');
+            if (p.length > 1) {
+                t += 60.0 * parseFloat(p[0]);
+                p.shift();
+            }
+            t += parseFloat(p[0]);
+            if (t == NaN)
+                return 0.0;
+            return t;
+        }
+
+        render() {
+            this.e_name.value = this.name;
+            this.e_time.value = Driver.timeToString(this.time);
+        }
+
+        change(e) {
+            this.name = this.e_name.value.replace(/[^A-Za-z\s]/g, '');
+            this.time = Driver.stringToTime(this.e_time.value);
+            this.e_li.setAttribute('id', this.name);
+            this.render();
+        }
+    }
+
+    window.lb = {
+        drivers: [],
+
+        add_driver: () => {
+            let driver = new Driver();
+            window.lb.drivers.push(driver);
+            document.querySelector("#drivers").appendChild(driver.e_li);
+        },
+        del_driver: (d) => {
+            d.e_li.remove();
+            window.lb.drivers = window.lb.drivers.filter((_d) => { return _d.name != d.name; });
+        },
+        sort_drivers: (e) => {
+            window.lb.drivers.sort((a,b) => {
+                return a.time - b.time;
+            });
+            let drivers = window.lb.drivers;
+            let sorted_elements = drivers.map((d) => {
+                d.render();
+                return d.e_li;
+            });
+            document.querySelector("#drivers").replaceChildren(...sorted_elements);
+            let names = drivers.map((d) => d.name);
+            let times = drivers.map((d) => d.time);
+            let times_str = times.map((t) => Driver.timeToString(t));
+            let n_width = Math.max(...names.map((v) => v.length));
+            let t_width = Math.max(...times_str.map((v) => v.length));
+            let v = "";
+            for (let i = 0; i < names.length; i++) {
+                if (i == 0) {
+                    delta = "---";
+                } else {
+                    delta = Driver.timeToString(times[i] - times[i - 1]);
+                    if (delta == 0.0) {
+                        delta = "---";
+                    }
+                }
+                const pad = "   ";
+                v += (i+1).toString() + ". " + names[i].padEnd(n_width, ' ') + pad + times_str[i].padStart(t_width, ' ') + pad + delta + "\n";
+            }
+            let tbl = document.querySelector('#table');
+            tbl.value = v;
+            tbl.setAttribute('rows', names.length);
+        },
+        parse_table: () => {
+            window.lb.drivers = [];
+            document.querySelector('#drivers').replaceChildren();
+            let lines = document.querySelector('#table').value.split("\n");
+            lines.forEach((l) => {
+                let p = l.match(/([0-9]+.)?\s*([^0-9:.]+)\s+([0-9:.]+).*/);
+                if (!p) {
+                    return;
+                }
+                window.lb.drivers.push(new Driver({
+                    name: p[2].trim(),
+                    time: Driver.stringToTime(p[3])
+                }));
+            });
+            window.lb.sort_drivers();
+        },
+        load: () => {
+            fetch('board').then((res) => {
+                res.json().then((board) => {
+                    window.lb.drivers = board.drivers.map((d) => new Driver(d));
+                    window.lb.sort_drivers();
+                });
+            });
+        },
+        save: () => {
+            let board = {
+                ver: 1,
+                drivers: window.lb.drivers.map((d) => { return {name: d.name, time: d.time} }),
+            };
+            fetch('board', {
+                method: "POST",
+                body: JSON.stringify(board),
+            });
+        },
+    };
+
+    document.querySelector("#add_driver").addEventListener('click', lb.add_driver);
+    document.querySelector("#sort_drivers").addEventListener('click', lb.sort_drivers);
+    document.querySelector("#parse_table").addEventListener('click', lb.parse_table);
+    document.querySelector("#load").addEventListener('click', lb.load);
+    document.querySelector("#save").addEventListener('click', lb.save);
+
+    window.lb.load();
+});
