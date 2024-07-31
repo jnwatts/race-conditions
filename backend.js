0766595fd60494509bb9f9e426ab9ab7738f505a
Josh Watts <josh+github@sroz.net>
2024-07-31 17:51:36 -0400

    Replace node:http with express

--- backend.js
+++ backend.js
@@ -1,8 +1,13 @@
-const http = require('node:http');
+const express = require('express');
 const fs = require('node:fs');
 const Ajv = require('ajv');
 const ajv = new Ajv();
-const board_file_name = "/var/lib/race/board.json";
+var static_path = process.env.STATIC_PATH;
+static_path ??= "/usr/share/race/";
+var board_file_name = process.env.BOARD_FILE;
+board_file_name ??= "/var/lib/race/board.json";
+var http_port = process.env.HTTP_PORT;
+http_port ??= 8000;
 const validate_board = ajv.compile({
 	type: "object",
 	properties: {
@@ -34,42 +39,20 @@ const validate_board = ajv.compile({
 	additionalProperties: false
 });
 
-function serve_file(res, fn, type) {
-	let st;
-	try {
-		st = fs.statSync(fn);
-	} catch (err) {
-		let empty = "{}";
-		res.writeHead(200, {
-			'Content-Type': type,
-			'Content-Length': empty.length,
-		});
-		res.write(empty);
-		res.end();
-		return;
-	}
-	res.writeHead(200, {
-		'Content-Type': type,
-		'Content-Length': st.size
-	});
-	var readStream = fs.createReadStream(fn);
-	readStream.pipe(res);
-}
-
-function write_file(req, res, fn) {
-	let body = null;
-	req.on('data', (chunk) => {
-		if (body == null) {
-			body = chunk.toString();
-			if (chunk.length > 2*1024) {
-				res.statusCode = 400;
-				res.end();
-			}
-		}
-	}).on('end', () => {
-		let board = JSON.parse(body);
+s = express();
+s.use(express.json());
+s.use(express.static(static_path));
+s.get('/', (req, res) => {
+	res.sendFile(static_path + "/index.html");
+});
+s.route('/board')
+	.get((req, res) => {
+		res.type('application/json').sendFile(board_file_name);
+	})
+	.post((req, res) => {
+		let board = req.body;
 		if (!validate_board(board)) {
-			res.statusCode = 400;
+			res.status(400);
 			res.end();
 			return;
 		}
@@ -78,35 +61,11 @@ function write_file(req, res, fn) {
 			return d;
 		});
 		console.log(Date.now(), 'WRITE', board);
-		fs.writeFileSync(fn, JSON.stringify(board));
-	});
-}
-
-const s = http.createServer();
-s.on('request', (req, res) => {
-	try {
-		console.log(Date.now(), req.method, req.url);
-		if (req.url == "/board") {
-			if (req.method == "GET") {
-				serve_file(res, board_file_name, "application/json");
-			} else if (req.method == "POST") {
-				write_file(req, res, board_file_name);
-			} else {
-				res.statusCode = 400;
-				res.end();
-			}
-		} else if (req.url == "/" || req.url == "/index.html") {
-			serve_file(res, "./index.html", "text/html");
-		} else if (req.url == "/index.js") {
-			serve_file(res, "./index.js", "application/javascript");
-		} else {
-			res.statusCode = 404;
-			res.end();
-		}
-	} catch(err) {
-		console.log(500, err);
-		res.statusCode = 500;
+		fs.writeFileSync(board_file_name, JSON.stringify(board));
+		res.status(200);
 		res.end();
-	}
-});
-s.listen(8000);
+	});
+s.listen(http_port);
+
+console.log("BOARD_FILE", board_file_name);
+console.log("PORT", http_port);
